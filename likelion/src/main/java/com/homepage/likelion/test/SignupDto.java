60c77eb34a7b8d19c135b3e150bc9054eb06858d
JuyeonLee <dlwndus708@naver.com>
2024-05-08 21:32:56 +0900

    ✨ GlobalExceptionHandler 작성

---
+++ likelion/src/main/java/com/homepage/likelion/test/SignupDto.java
@@ -0,0 +1,22 @@
+package com.homepage.likelion.test;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupDto {
+
+    @NotEmpty(message = "userId는 필수값입니다.")
+    private String userId;
+
+    @Email(message = "이메일 형식을 맞춰주세요")
+    private String email;
+    private String password;
+
+
+
+}
