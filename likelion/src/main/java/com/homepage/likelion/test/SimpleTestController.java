59ae6f654bfc6ddd5aed6560c76054fadf74277d
JuyeonLee <dlwndus708@naver.com>
2024-05-08 15:34:37 +0900

    ✨ 간단한 테스트 컨트롤러 작성

---
+++ likelion/src/main/java/com/homepage/likelion/test/SimpleTestController.java
@@ -0,0 +1,23 @@
+package com.homepage.likelion.test;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//SimpleTestController가 restroller..? 웹서비스임을 나타내는 어노테이션
+@RequestMapping("/api/simpleText")
+public class SimpleTestController {
+    //GET http://localhost:8080/api/simpleText/success
+
+    @GetMapping("/success")
+    public String simpleTextSuccess() {
+        return "안녕";
+    }
+
+    @GetMapping("/fail")
+    public ResponseEntity<String> simpleTextFail() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("실패");
+    }
+}
