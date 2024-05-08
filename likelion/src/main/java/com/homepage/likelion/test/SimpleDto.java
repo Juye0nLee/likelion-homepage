a94e9c923320e291e209e4b87af623414a7b0d5e
JuyeonLee <dlwndus708@naver.com>
2024-05-08 16:30:01 +0900

    ✨ CustomApiResponse를 활용한 Controller 작성

---
+++ likelion/src/main/java/com/homepage/likelion/test/SimpleDto.java
@@ -0,0 +1,14 @@
+package com.homepage.likelion.test;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SimpleDto {
+
+    private String userId;
+    private String email;
+}
