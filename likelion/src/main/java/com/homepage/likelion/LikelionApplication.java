af9dac0f389f7317a82477a91471488e8be69ca5
JuyeonLee <dlwndus708@naver.com>
2024-05-08 14:28:38 +0900

    🎉 프로젝트 시작

---
+++ likelion/src/main/java/com/homepage/likelion/LikelionApplication.java
@@ -0,0 +1,16 @@
+//이 application의 시작점
+//스프링부트 프로젝트 생성 시 자동으로 만들어짐
+
+package com.homepage.likelion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelionApplication.class, args);
+	}
+
+}
